226378a55a04876447384cc9909b4c53e3ab619c
Lâm <lam.ledinh31@gmail.com>
2020-10-28 11:20:35 +0100

    Ajout test exception enregistrer arrive

--- test_labo.py
+++ test_labo.py
@@ -4,6 +4,7 @@ from labo import labo_vide
 from labo import enregistrer_arrive 
 from labo import enregistrer_depart 
 from labo import changer_bureau 
+from labo import PresentException 
 
 def test_labo_vide():
     labo = labo_vide()
@@ -13,6 +14,17 @@ def test_enregistrer_arrive():
     labo = labo_vide()
     enregistrer_arrive(labo, "Xavier", "F305")
     assert labo["Xavier"] == "F305"
+
+
+def test_enregistrer_arrive_fail():
+    labo = labo_vide()
+    enregistrer_arrive(labo, "Xavier", "F305")
+    assert labo["Xavier"] == "F305"
+    try:
+        enregistrer_arrive(labo, "Xavier", "F355")
+        assert False
+    except PresentException as e:
+        assert True
 
 def test_enregistrer_depart():
     labo = labo_vide()
